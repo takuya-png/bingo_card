36575ff2d2a94559965ef57c5a7e70d21c86f945
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-12-12 13:13:50 +0900

    リファクタリング2

--- bingo_card.rb
+++ bingo_card.rb
@@ -1,14 +1,11 @@
-def word
-  [' B', ' I', ' N', ' G', ' O'].join(' | ')
-end
-
-def numbers
-  bingos = (1..75).each_slice(15).map { |bingo| bingo.sample(5) }.transpose
-  bingos[2][2] = "  "
-  bingos.map  { |bin|
-    bin.map { |b| b.to_s.rjust(2) }.join(" | ")
+def card
+  bingo = ["BINGO".chars]
+  numbers = (1..75).each_slice(15).map { |number| number.sample(5) }.transpose
+  numbers[2][2] = "  "
+  rows = bingo + numbers
+  rows.map  { |row|
+    row.map { |r| r.to_s.rjust(2) }.join(" | ")
   }.join("\n")
 end
 
-puts word
-puts numbers
+puts card
